ebf793891727437895a414746520473f9fec5a42
Wesley de Groot <wes@vista.aero>
2022-03-27 19:09:46 +0200

    Init

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+// This is a basic PHP File.
+
+function doSomething($normal = "Yes") {
+    // Do someting normal.
+    // Tab is 4
+    // Ok
+    // Tab is 2
+  // 2 ?
+  // 2?
+    // 2
+    // 2
+  
+    
+}
